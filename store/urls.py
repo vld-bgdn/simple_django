222f0e87de278fd17b6be0ef2f4cb0af51177929
Vladimir Bogdanov <vld.bgdn@icloud.com>
2025-05-31 11:12:04 +0300

    add github action with tests

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path("", views.ProductListView.as_view(), name="product_list"),
     path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
     path("product/new/", views.ProductCreateView.as_view(), name="product_new"),
-    path("product/<int:pk>/edit/", views.ProductUpdateView.as_view(), name="product_edit"),
-    path("product/<int:pk>/delete/", views.ProductDeleteView.as_view(), name="product_delete"),
+    path(
+        "product/<int:pk>/edit/", views.ProductUpdateView.as_view(), name="product_edit"
+    ),
+    path(
+        "product/<int:pk>/delete/",
+        views.ProductDeleteView.as_view(),
+        name="product_delete",
+    ),
 ]
